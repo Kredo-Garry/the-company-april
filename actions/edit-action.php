9f4ebf3440e67f4fe99405d99ba040bd0fc3c9f5
Kredo-Garry <kredoteacher.grodriguez@gmail.com>
2023-04-17 19:19:23 +0800

    The-company first upload

---
+++ actions/edit-action.php
@@ -0,0 +1,9 @@
+<?php
+    include '../classes/User.php';
+
+    # Instantiate and object
+    $user = new User;
+
+    # Call the method update()
+    $user->update($_POST, $_FILES);
+?>
---
+++ actions/login-action.php
@@ -0,0 +1,9 @@
+<?php
+    include '../classes/User.php';
+
+    # Create an object
+    $user = new User;
+
+    # Call the object
+    $user->login($_POST);
+?>
